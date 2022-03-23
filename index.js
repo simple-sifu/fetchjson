c76dbbb7bae6fefe936aab2642f09c02059c4c63
Tommy Han <tommy_han1@intuit.com>
2022-03-23 00:37:09 -0400

    refactor

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    // Response.data has properties of:
+    // id
+    // title
+    // finished
+    var todo = response.data;
+    console.log("todo =", todo);
+    var ID = todo.ID;
+    var title = todo.Title;
+    var finished = todo.finished;
+    console.log("\n    The Todo with ID:".concat(ID, "\n    has a title of: ").concat(title, "\n    Is it finished? ").concat(finished, "\n  "));
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import axios from 'axios';
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response =>{
-  const todo = response.data;
+  // Response.data has properties of:
+  // id
+  // title
+  // completed
+  const todo = response.data as Todo;
+  console.log("todo =", todo);
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
 
   console.log(`
     The Todo with ID:${ID}
